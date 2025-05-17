8db63504021ee0aa91f5b28a3ac9fa6cec127d1d
Mustafa Keser <mustafakeser0@hotmail.com>
2025-05-17 12:40:19 +0300

    Add button to HomePage for initiating tests; enhance user interaction

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,6 +11,12 @@ function HomePage() {
         <input className="w-60 h-9 rounded-md placeholder:text-sm " name="age" type="text" placeholder="Lütfen yaşınızı giriniz..."/>
         <label htmlFor="phoneNo">Tel-no: </label>
         <input className="w-60 h-9 rounded-md placeholder:text-sm " name="phoneNo" type="text" placeholder="Lütfen numaranızı giriniz..."/>
+        <button
+         
+          className="mb-8 mt-4 bg-[#5BA87C] rounded-md text-white px-4 py-2 hover:cursor hover:bg-[#4A9B6D]"
+        >
+          Teste Git
+        </button>
       </form>
     </div>
   )
