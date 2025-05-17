dc3534199057b5c8df0db0a432e531977bc9834e
Mustafa Keser <mustafakeser0@hotmail.com>
2025-05-17 12:50:48 +0300

    Enhance input fields in HomePage with improved styling and type attributes for better user experience

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,19 @@
 
 
 function HomePage() {
+
+
+
   return (
     <div className='flex flex-col items-center h-screen bg-[#F6F6F6]'>
       <p>Hoşgeldiniz</p>
       <form className="flex flex-col mt-8 gap-2">
         <label htmlFor="name">Ad-Soyad: </label>
-        <input className="w-60 h-9 rounded-md placeholder:text-sm " name="name" type="text" placeholder="Lütfen adınızı giriniz..."/>
+        <input className="w-60 h-9 rounded-md placeholder:text-sm pl-2" name="name" type="text" placeholder="Lütfen adınızı giriniz..."/>
         <label htmlFor="age">Yaş: </label>
-        <input className="w-60 h-9 rounded-md placeholder:text-sm " name="age" type="text" placeholder="Lütfen yaşınızı giriniz..."/>
+        <input className="w-60 h-9 rounded-md placeholder:text-sm pl-2" name="age" type="number" placeholder="Lütfen yaşınızı giriniz..."/>
         <label htmlFor="phoneNo">Tel-no: </label>
-        <input className="w-60 h-9 rounded-md placeholder:text-sm " name="phoneNo" type="text" placeholder="Lütfen numaranızı giriniz..."/>
+        <input className="w-60 h-9 rounded-md placeholder:text-sm pl-2" name="phoneNo" type="tel" placeholder="05xx xxx xx xx"/>
         <button
          
           className="mb-8 mt-4 bg-[#5BA87C] rounded-md text-white px-4 py-2 hover:cursor hover:bg-[#4A9B6D]"
