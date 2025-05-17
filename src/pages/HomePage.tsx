b59842bf5264dfcab3a2de90f795df13fa527b01
Mustafa Keser <mustafakeser0@hotmail.com>
2025-05-17 12:54:43 +0300

    Enhance input fields in HomePage with improved background color for better visibility

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,11 +9,11 @@ function HomePage() {
       <p>Hoşgeldiniz</p>
       <form className="flex flex-col mt-8 gap-2">
         <label htmlFor="name">Ad-Soyad: </label>
-        <input className="w-60 h-9 rounded-md placeholder:text-sm pl-2" name="name" type="text" placeholder="Lütfen adınızı giriniz..."/>
+        <input className="w-60 h-9 rounded-md placeholder:text-sm pl-2 bg-[#F0F0F0]" name="name" type="text" placeholder="Lütfen adınızı giriniz..."/>
         <label htmlFor="age">Yaş: </label>
-        <input className="w-60 h-9 rounded-md placeholder:text-sm pl-2" name="age" type="number" placeholder="Lütfen yaşınızı giriniz..."/>
+        <input className="w-60 h-9 rounded-md placeholder:text-sm pl-2 bg-[#F0F0F0]" name="age" type="number" placeholder="Lütfen yaşınızı giriniz..."/>
         <label htmlFor="phoneNo">Tel-no: </label>
-        <input className="w-60 h-9 rounded-md placeholder:text-sm pl-2" name="phoneNo" type="tel" placeholder="05xx xxx xx xx"/>
+        <input className="w-60 h-9 rounded-md placeholder:text-sm pl-2 bg-[#F0F0F0]" name="phoneNo" type="tel" placeholder="05xx xxx xx xx"/>
         <button
          
           className="mb-8 mt-4 bg-[#5BA87C] rounded-md text-white px-4 py-2 hover:cursor hover:bg-[#4A9B6D]"
